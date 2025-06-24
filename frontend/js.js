773ccd4f7e058ccbd2630bed6119c344bf43039f
joaovpt <joaotassi18@gmail.com>
2025-06-24 14:25:28 -0300

    chore: External IP

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
+        url: "http://10.106.147.228",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
